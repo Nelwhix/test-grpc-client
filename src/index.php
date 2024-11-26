675f1002ebac1f5a1bd5ce05e36d26948cc58c7a
Nelson Isioma <nelsonisioma1@gmail.com>
2024-11-26 04:53:56 +0100

    works

--- src/index.php
+++ src/index.php
@@ -7,24 +7,19 @@ namespace Nelwhix\ClientPHP;
 use Chat\ChatServiceClient;
 use Chat\Message;
 use Grpc\ChannelCredentials;
-use Illuminate\Support\Collection;
 
 require __DIR__ . '/../vendor/autoload.php';
-
-//$coll = new Collection(['hello', 'world']);
-//
-//$coll->each(function ($name) {
-//    var_dump($name);
-//});
-//die();
 
 $client = new ChatServiceClient('localhost:9000', [
     'credentials' => ChannelCredentials::createInsecure(),
 ]);
 
 $message = new Message([
-    'Body' => 'Hi from PHP!'
+    'body' => 'Hi from PHP!'
 ]);
 
 $response = $client->SayHello($message)->wait();
-var_dump($response);
+
+/** @var Message $responseMsg */
+$responseMsg = $response[0];
+echo "Response from server is: " . $responseMsg->getBody() . "\n";
